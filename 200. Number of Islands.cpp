87ff277d0b3768cd64692102360cc3e09066e040
theujjwalsingh18 <theujjwalsinghh@gmail.com>
2024-04-19 22:20:50 +0530

    init

---
+++ 200. Number of Islands.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int m, n;
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '#';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        int island = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    island++;
+                }
+            }
+        }
+        return island;
+    }
+};
