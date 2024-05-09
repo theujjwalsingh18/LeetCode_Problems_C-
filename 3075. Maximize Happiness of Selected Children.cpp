5f1f09d027c54e53f00c7507989e6c1b1ca78424
theujjwalsingh18 <theujjwalsinghh@gmail.com>
2024-05-09 18:56:30 +0530

    init

---
+++ 3075. Maximize Happiness of Selected Children.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        long long result = 0;
+        for (int i = 0; i < k; i++) {
+            result += max(happiness[i] - i, 0);
+        }
+        return result;
+    }
+};
