7fb8d66c57ff7299d6acf1edb78acfbe651ee82a
Ujjwal Kumar Singh <132118210+theujjwalsingh18@users.noreply.github.com>
2024-05-17 17:07:41 +0530

    init

---
+++ 2331. Evaluate Boolean Binary Tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+
+ public:
+
+  bool evaluateTree(TreeNode* root) {
+
+    if (root->val < 2)
+
+      return root->val;
+
+    if (root->val == 2)  // OR
+
+      return evaluateTree(root->left) || evaluateTree(root->right);
+
+
+
+    return evaluateTree(root->left) && evaluateTree(root->right);
+
+  }
+
+};
