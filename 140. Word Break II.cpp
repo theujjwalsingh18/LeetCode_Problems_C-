4f7b8b7b2cf5e12b867aa7a8bea17f41fb1c92e2
theujjwalsingh18 <theujjwalsinghh@gmail.com>
2024-05-26 07:40:08 +0530

    init

---
+++ 140. Word Break II.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    unordered_set<string> dict;
+    unordered_map<string, vector<string>> mp;
+
+    vector<string> solve(string &s) {
+       if(s.empty()) {
+            return {""};
+       }
+      
+       if(mp.count(s)){
+            return mp[s];
+        }
+        
+        vector<string> result;
+        for(int l = 1; l <= s.length(); l++) {
+            string currWord   = s.substr(0, l);
+            if(dict.count(currWord)) {
+                string remainWord = s.substr(l);
+                vector<string> remainResult = solve(remainWord);
+                for(string &w : remainResult) {
+                    string toAdd = currWord + (w.empty() ? "" : " ") + w;
+                    result.push_back(toAdd);
+                }
+            }
+        }
+
+        return mp[s] = result;
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        for(string &word : wordDict) {
+            dict.insert(word);
+        }
+        
+        return solve(s);
+    }
+};
+
+
